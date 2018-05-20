0c0c5a0f7f16590c1f1814df4bc6225e15cf5e77
nttri <bem0302@gmail.com>
2018-05-20 18:30:21 +0700

    update Helper

--- KaraokeManagement/src/common/Helper.java
+++ KaraokeManagement/src/common/Helper.java
@@ -33,8 +33,8 @@ public class Helper {
         donThanhToan.setMaKhachHang(rs.getInt("MAKH"));
         donThanhToan.setMaPhong(rs.getInt("MAPHONG"));
         donThanhToan.setGiaPhong(rs.getInt("GIAPHONG"));
-        donThanhToan.setThoiGianBatDau(rs.getDate("THOIGIANBD"));
-        donThanhToan.setThoiGianKetThuc(rs.getDate("THOIGIANKT"));
+        donThanhToan.setThoiGianBatDau(rs.getTimestamp("THOIGIANBD"));
+        donThanhToan.setThoiGianKetThuc(rs.getTimestamp("THOIGIANKT"));
         donThanhToan.setMaKhuyenMai(rs.getString("MAKM"));
         donThanhToan.setTinhTrang(rs.getBoolean("TINHTRANG"));
     }
