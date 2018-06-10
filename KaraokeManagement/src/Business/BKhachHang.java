fc797846df00c0a01f4382eead1bfe14c71ea41c
nttri <bem0302@gmail.com>
2018-06-10 17:35:21 +0700

    make BKhachHang used DAO

--- KaraokeManagement/src/Business/BKhachHang.java
+++ KaraokeManagement/src/Business/BKhachHang.java
@@ -5,59 +5,112 @@
  */
 package Business;
 
+import DAO.DAOKhachHang;
 import common.Helper;
+import common.MyStrings;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import javax.swing.JOptionPane;
 import model.KhachHang;
 
 /**
  *
  * @author Thoai
  */
-public class BKhachHang extends Business{
+public class BKhachHang extends Business {
+
     String sql;
     ResultSet rs;
-    
-    public BKhachHang(){
+    DAOKhachHang DKhachHang = new DAOKhachHang();
+
+    public BKhachHang() {
         super();
     }
-       
+
     public ArrayList<KhachHang> layTatCaKhachHang() throws SQLException {
-        ArrayList<KhachHang> arrKhachHang= new ArrayList();
-        sql = "layTatCaKhachHang";
-        rs = data.fetchData(sql);
-        
-        while (rs.next()) {
-            KhachHang khachHang = new KhachHang();
-            Helper.setKhachHang(khachHang, rs);
-            arrKhachHang.add(khachHang);
-        }
-        return arrKhachHang;
+        return DKhachHang.layTatCaKhachHang();
     }
-    
+
     public KhachHang layKhachHangTheoMa(int maKH) throws SQLException {
-        KhachHang khachHang = new KhachHang();
-        sql = "layKhachHangTheoMa (" + maKH + ")";
-        rs = data.fetchData(sql);
-        if (rs.next()){
-            Helper.setKhachHang(khachHang, rs);
-        }         
-        return khachHang;
+        return DKhachHang.layKhachHangTheoMa(maKH);
     }
-    
+
+    Boolean isOldEnough(String input) {
+        Date _today = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(_today);
+        int _namHienTai = cal.get(Calendar.YEAR);
+
+        Date _ngaySinh = new Date();
+        try {
+            _ngaySinh = sdf.parse(input);
+        } catch (ParseException ex) {
+            //
+        }
+        cal.setTime(_ngaySinh);
+        int _namSinh = cal.get(Calendar.YEAR);
+
+        if (_namHienTai - _namSinh < 16) {
+            return false;
+        }
+        return true;
+    }
+
     public boolean themKhachHang(String hoTen, String gioiTinh, String ngaySinh, String diaChi, String cmnd, String sdt) throws SQLException {
-        sql = "themKhachHang (N'" + hoTen + "', N'" + gioiTinh + "', '" + ngaySinh + "', N'" + diaChi + "', '" + cmnd + "', '" + sdt + "')";
-        return data.Execute(sql);
+        if (cmnd.length() != 9) {
+            JOptionPane.showMessageDialog(null, MyStrings.Invalid_CMND);
+            return false;
+        }
+        if (sdt.length() < 10) {
+            JOptionPane.showMessageDialog(null, MyStrings.Invalid_Phone);
+            return false;
+        }
+        if (sdt.length() == 10 && !sdt.startsWith("09")) {
+            JOptionPane.showMessageDialog(null, MyStrings.Invalid_Phone);
+            return false;
+        }
+        if (sdt.length() == 11 && !sdt.startsWith("01")) {
+            JOptionPane.showMessageDialog(null, MyStrings.Invalid_Phone);
+            return false;
+        }
+        if (!isOldEnough(ngaySinh)) {
+            JOptionPane.showMessageDialog(null, MyStrings.Invalid_Birthday);
+            return false;
+        }
+        return DKhachHang.themKhachHang(hoTen, gioiTinh, ngaySinh, diaChi, cmnd, sdt);
     }
-    
+
     public boolean capNhatKhachHang(int ma, String hoTen, String gioiTinh, String ngaySinh, String diaChi, String cmnd, String sdt) throws SQLException {
-        sql = "capNhatKhachHang (" + ma + ", N'"+ hoTen + "', N'" + gioiTinh + "', '" + ngaySinh + "', N'" + diaChi + "', '" + cmnd + "', '" + sdt + "')";
-        return data.Execute(sql);
+        if (cmnd.length() != 9) {
+            JOptionPane.showMessageDialog(null, MyStrings.Invalid_CMND);
+            return false;
+        }
+        if (sdt.length() < 10) {
+            JOptionPane.showMessageDialog(null, MyStrings.Invalid_Phone);
+            return false;
+        }
+        if (sdt.length() == 10 && !sdt.startsWith("09")) {
+            JOptionPane.showMessageDialog(null, MyStrings.Invalid_Phone);
+            return false;
+        }
+        if (sdt.length() == 11 && !sdt.startsWith("01")) {
+            JOptionPane.showMessageDialog(null, MyStrings.Invalid_Phone);
+            return false;
+        }
+        if (!isOldEnough(ngaySinh)) {
+            JOptionPane.showMessageDialog(null, MyStrings.Invalid_Birthday);
+            return false;
+        }
+        return DKhachHang.capNhatKhachHang(ma, hoTen, gioiTinh, ngaySinh, diaChi, cmnd, sdt);
     }
-    
+
     public boolean xoaKhachHang(int ma) throws SQLException {
-        sql = "xoaKhachHang (" + ma + "')";
-        return data.Execute(sql);
+        return DKhachHang.xoaKhachHang(ma);
     }
 }
